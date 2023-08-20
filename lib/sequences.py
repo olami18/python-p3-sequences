8a85c2548a0e7f9a70d5e21424aaeadc9b893c9f
+ <+>
2023-08-20 19:14:22 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+
+    if length >= 1:
+        fibonacci_sequence.append(0)
+    if length >= 2:
+        fibonacci_sequence.append(1)
+
+
+    while len(fibonacci_sequence) < length:
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_number)
+
+    print(fibonacci_sequence)
+
+# Call the function with the desired length
+print_fibonacci(10)  # Replace 10 with the desired length
